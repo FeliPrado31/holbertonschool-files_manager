a3d942c7a922bc579eda471159cd0ad1fea0f640
Feli <srgatoiscool@gmail.com>
2021-01-31 23:17:00 -0500

    :sparkles: add

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,31 +1,51 @@
-const sha1 = require('sha1');
-const dbClient = require('../utils/db');
+import sha1 from 'sha1';
+import DBClient from '../utils/db';
+import RedisClient from '../utils/redis';
 
-/**
- * postNew - callback for route POST /users
- */
-async function postNew(req, res) {
-  const { email, password } = req.body;
+const { ObjectId } = require('mongodb');
 
-  if (!email) res.status(400).json({ error: 'Missing email' });
-  if (!password) res.status(400).json({ error: 'Missing password ' });
+class UsersController {
+  static async postNew(request, response) {
+    const userEmail = request.body.email;
+    if (!userEmail)
+      return response.status(400).send({ error: 'Missing email' });
 
-  // check if email already exists in db
-  const found = await dbClient.client
-    .collection('users')
-    .find({ email })
-    .count();
-  if (found > 0) return res.status(400).json({ error: 'Already exist' });
-  // encrypt password and insert user in datbase
-  const user = await dbClient.client
-    .collection('users')
-    .insertOne({ email, password: sha1(password) });
-  if (user) {
-    return res
+    const userPassword = request.body.password;
+    if (!userPassword)
+      return response.status(400).send({ error: 'Missing password' });
+
+    const oldUserEmail = await DBClient.db
+      .collection('users')
+      .findOne({ email: userEmail });
+    if (oldUserEmail)
+      return response.status(400).send({ error: 'Already exist' });
+
+    const shaUserPassword = sha1(userPassword);
+    const result = await DBClient.db
+      .collection('users')
+      .insertOne({ email: userEmail, password: shaUserPassword });
+
+    return response
       .status(201)
-      .json({ id: user.ops[0]._id, email: user.ops[0].email });
+      .send({ id: result.insertedId, email: userEmail });
   }
-  return res.status(500).json({ error: 'Could not create user' });
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token') || null;
+    if (!token) return response.status(401).send({ error: 'Unauthorized' });
+
+    const redisToken = await RedisClient.get(`auth_${token}`);
+    if (!redisToken)
+      return response.status(401).send({ error: 'Unauthorized' });
+
+    const user = await DBClient.db
+      .collection('users')
+      .findOne({ _id: ObjectId(redisToken) });
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    delete user.password;
+
+    return response.status(200).send({ id: user._id, email: user.email });
+  }
 }
 
-module.exports = { postNew };
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,49 @@
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const express = require('express');
-const { postNew } = require('../controllers/UsersController');
-
+// all endpoints of our API
 const router = (app) => {
   const route = express.Router();
   app.use(express.json());
   app.use('/', route);
 
-  // app routes
-  route.get('/status', (request, response) => AppController.getStatus(request, response));
-
-  route.get('/stats', (request, response) => AppController.getStats(request, response));
-
-  // user routes
-  router.post('/users', (req, res) => postNew(req, res));
+  route.get('/status', (request, response) =>
+    AppController.getStatus(request, response)
+  );
+  route.get('/stats', (request, response) =>
+    AppController.getStats(request, response)
+  );
+  route.post('/users', (request, response) =>
+    UsersController.postNew(request, response)
+  );
+  route.get('/connect', (request, response) =>
+    AuthController.getConnect(request, response)
+  );
+  route.get('/disconnect', (request, response) =>
+    AuthController.getDisconnect(request, response)
+  );
+  route.get('/users/me', (request, response) =>
+    UsersController.getMe(request, response)
+  );
+  route.post('/files', (request, response) =>
+    FilesController.postUpload(request, response)
+  );
+  route.get('/files/:id', (request, response) =>
+    FilesController.getShow(request, response)
+  );
+  route.get('/files', (request, response) =>
+    FilesController.getIndex(request, response)
+  );
+  route.put('/files/:id/publish', (request, response) =>
+    FilesController.putPublish(request, response)
+  );
+  route.put('/files/:id/unpublish', (request, response) =>
+    FilesController.putUnpublish(request, response)
+  );
+  route.get('/files/:id/data', (request, response) =>
+    FilesController.getFile(request, response)
+  );
 };
+
 export default router;
