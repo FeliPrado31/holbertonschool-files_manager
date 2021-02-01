2f5e970047349cdac37d7ae03318a69875bfb956
Feli <srgatoiscool@gmail.com>
2021-01-31 23:01:03 -0500

    :sparkles: update

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,19 @@
-import AppController from "../controllers/AppController";
-const { postNew } = require("../controllers/UsersController");
+import AppController from '../controllers/AppController';
 
-const express = require("express");
+const express = require('express');
+const { postNew } = require('../controllers/UsersController');
 
 const router = (app) => {
   const route = express.Router();
   app.use(express.json());
-  app.use("/", route);
+  app.use('/', route);
 
   // app routes
-  route.get("/status", (request, response) =>
-    AppController.getStatus(request, response)
-  );
+  route.get('/status', (request, response) => AppController.getStatus(request, response));
 
-  route.get("/stats", (request, response) =>
-    AppController.getStats(request, response)
-  );
+  route.get('/stats', (request, response) => AppController.getStats(request, response));
 
   // user routes
-  router.post("/users", (req, res) => postNew(req, res));
+  router.post('/users', (req, res) => postNew(req, res));
 };
 export default router;
